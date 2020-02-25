b1801350e9b61178eecf8ecbe8032c9afdf8d76b
james <yanmz_vip@163.com>
2020-02-25 23:45:13 +0800

    JVM 常量在底层的运作基理

---
+++ ssy-study-jvm/src/main/java/com/classloader/MyTest1.java
@@ -0,0 +1,26 @@
+package com.classloader;
+
+/**
+ * 对于静态字段来说，只有直接定义了该字段的类才会被初始化
+ * 当一个类在初始化时，要求其父类全部都已经初始化完毕了
+ * -XX:+TraceClassLoading 用于追踪类的加载信息并打印出来
+ */
+public class MyTest1 {
+    public static void main(String[] args) {
+        System.out.println(MyChild1.str);
+    }
+}
+
+class MyParent1{
+    public static  String str = "hello  world";
+    static {
+        System.out.println("MyParent1 static block...");
+    }
+}
+
+class  MyChild1 extends  MyParent1{
+    public  static  String str2 = "welcome";
+    static {
+        System.out.println("MyChild1 static block...");
+    }
+}
