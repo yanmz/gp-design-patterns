f8352348693a430200770c7c60f024dd3006039d
james <yanmz_vip@163.com>
2020-05-25 23:32:24 +0800

    数据结构

---
+++ juc-mode/src/main/java/com/juc/gp/ThreadLocalDemo.java
@@ -0,0 +1,33 @@
+package com.juc.gp;
+
+
+public class ThreadLocalDemo {
+
+//    private static int num=0;
+    static ThreadLocal<Integer> local=new ThreadLocal<Integer>(){
+        protected Integer initialValue(){
+            return 0; //初始化一个值
+        }
+    };
+    static ThreadLocal<Integer> local1=new ThreadLocal<Integer>(){
+        protected Integer initialValue(){
+            return 0; //初始化一个值
+        }
+    };
+
+    public static void main(String[] args) {
+        Thread[] thread=new Thread[5];
+        for (int i=0;i<5;i++){
+            thread[i]=new Thread(()->{
+                local1.set(10);
+                int num=local.get(); //获得的值都是0
+                local.set(num+=5); //设置到local中  thread[0] ->thread[1] ->
+                System.out.println(Thread.currentThread().getName()+"-"+num);
+                local.remove();
+            });
+        }
+        for (int i = 0; i < 5; i++) {
+            thread[i].start();
+        }
+    }
+}
